10d37b92bc338091b617e76bda2cd07da5f89dcc
João Gabriel <87152738+Moura14@users.noreply.github.com>
2025-05-06 08:46:03 -0300

    fix: correção no componente minefield

--- app/(tabs)/componentes/mineField.jsx
+++ app/(tabs)/componentes/mineField.jsx
@@ -3,12 +3,14 @@ import {View, StyleSheet} from 'react-native'
 import Field from './field'
 
 
+
+
 export default props => {
     const rows = props.board.map((row, r) => {
         const columns = row.map((field, c) => {
             return <Field {...field} key={c}></Field>
         })
-        return <View key={r}>{columns}</View>
+        return <View key={r} style={{flexDirection: 'row'}}>{columns}</View>
     })
     return <View style={styles.container}>{rows}</View>
 }
@@ -16,7 +18,6 @@ export default props => {
 
 const styles = StyleSheet.create({
     container: {
-        flexDirection: 'row',
         backgroundColor: '#EEE '
     }
 })
