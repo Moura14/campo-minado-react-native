8a18a5498aec9f35e944d0c9fa88bfbc8e1a1fea
João Gabriel <87152738+Moura14@users.noreply.github.com>
2025-05-10 15:29:39 -0300

    feat: lógica do jogo #02

--- app/(tabs)/functions.jsx
+++ app/(tabs)/functions.jsx
@@ -37,4 +37,35 @@ const createMineBoard = (rows, columns, minesAmount) => {
 }
 
 
+const cloneBoard = board => {
+    return board.map(row => {
+        return row.map(field => {
+            return {...field}
+        })
+    })
+}
+
+const getNeighbors = (board, row, column) => {
+    const neighbors = []
+    const rows = [row - 1, row + 1]
+    const columns = [column - 1, column + 1]
+    row.forEach(r => {
+        columns.forEach(c => {
+            const diferent = r !== row || c !== column
+            const validRow = r>= 0 && board.length
+            const validColumn = c >= 0 && c < board[0].length
+            if(diferent && validRow && validColumn){
+                neighbors.push(board[r][c])
+            }
+        })
+    })
+    return neighbors
+}
+
+
+const safeNeighborhood = (board, row, column) => {
+    const safes = (result, neighbor) => result && !neighbor.mined
+    return getNeighbors(board, row, column).reduce(safes, true)
+}
+
 export {createMineBoard}
